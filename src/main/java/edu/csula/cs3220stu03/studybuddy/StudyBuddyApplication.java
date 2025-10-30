3c7803b7a54e01bc1eb409746af31c758182ec0d
Diep <rdiep5@calstatela.edu>
2025-10-30 13:42:40 -0700

    Initial commit

---
+++ src/main/java/edu/csula/cs3220stu03/studybuddy/StudyBuddyApplication.java
@@ -0,0 +1,13 @@
+package edu.csula.cs3220stu03.studybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/csula/cs3220stu03/studybuddy/StudyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.csula.cs3220stu03.studybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
